0d463d4536d6e41e28c8b4e904b7da7bffa02b92
Umut Arpidinov <ymytbek0314@gmail.com>
2023-09-25 18:31:18 +0600

    add custom note categoryView

--- app/src/main/java/com/example/notesapp/ui/views/CustomNotesCategoryView.kt
+++ app/src/main/java/com/example/notesapp/ui/views/CustomNotesCategoryView.kt
@@ -1,32 +1,72 @@
 package com.example.notesapp.ui.views
 
 import android.content.Context
+import android.content.res.ColorStateList
+import android.graphics.Color
+import android.graphics.drawable.Drawable
 import android.util.AttributeSet
 import android.view.LayoutInflater
+import android.widget.LinearLayout
+import android.widget.TextView
 import androidx.cardview.widget.CardView
+import androidx.constraintlayout.widget.ConstraintLayout
+import androidx.core.view.ViewCompat
 import com.example.notesapp.R
 import com.example.notesapp.databinding.CustomCategoryVeiwBinding
 
-class CustomNotesCategoryView : CardView {
-    private val binding = CustomCategoryVeiwBinding.inflate(
-        LayoutInflater.from(context),
-        this, true
-    )
+class CustomNotesCategoryView(
+    context: Context,
+    attrs: AttributeSet?,
+    defStyleAttr: Int,
+    defStyleRes: Int
+) : LinearLayout(context, attrs, defStyleAttr, defStyleRes) {
 
-    constructor(context: Context) : super(context)
+    private val binding: CustomCategoryVeiwBinding
 
-    constructor(context: Context, attributeSet: AttributeSet) : super(context, attributeSet) {
-        context.obtainStyledAttributes(attributeSet, R.styleable.CustomNotesCategoryView).run {
-            getText(R.styleable.CustomNotesCategoryView_category_name)?.let {
-                setCategoryName(it.toString())
-            }
-            recycle()
+    constructor(context: Context, attrs: AttributeSet?, defStyleAttr: Int) : this(context, attrs, defStyleAttr, 0)
+    constructor(context: Context, attrs: AttributeSet?) : this(context, attrs, 0)
+    constructor(context: Context) : this(context, null)
+
+    init {
+        val inflater = LayoutInflater.from(context)
+        inflater.inflate(R.layout.custom_category_veiw, this, true)
+        binding = CustomCategoryVeiwBinding.bind(this)
+        initAttrs(attrs,defStyleAttr,defStyleRes)
+    }
+
+    private fun initAttrs(attrs: AttributeSet?, defStyleAttr: Int, defStyleRes: Int) {
+        if (attrs == null) return
+        val typedArray = context.obtainStyledAttributes(attrs, R.styleable.CustomNotesCategoryView, defStyleAttr, defStyleRes)
+        with(binding){
+            val text = typedArray.getString(R.styleable.CustomNotesCategoryView_category_name)
+            val drawable = typedArray.getDrawable(R.styleable.CustomNotesCategoryView_category_icon)
+            val iconBackgroundColor = typedArray.getColor(R.styleable.CustomNotesCategoryView_category_color,-1)
+            val notesCategoryBackgroundColor = typedArray.getColor(R.styleable.CustomNotesCategoryView_category_background,-1)
+
+            setNotesCategoryIcon(drawable)
+            setNotesCategoryText(text)
+            setIconBackgroundColor(iconBackgroundColor)
+            setNotesCategoryBackgroundColor(notesCategoryBackgroundColor)
+
         }
+
+        typedArray.recycle()
+
     }
 
-    fun setCategoryName(categoryName: String) {
-        binding.tvNotesCategory.text = categoryName
+    private fun setNotesCategoryIcon(drawable: Drawable?){
+        binding.ivCategoryNote.setImageDrawable(drawable)
     }
+    private fun setNotesCategoryText(text: String?){
+        binding.tvNotesCategory.text = text
+    }
+    private fun setIconBackgroundColor(color: Int){
+        binding.ivCategoryNote.setBackgroundColor(color)
+    }
+    private fun setNotesCategoryBackgroundColor(color: Int){
+        ViewCompat.setBackgroundTintList(binding.linearParent, ColorStateList.valueOf(color))
+    }
+
 
 
 }
